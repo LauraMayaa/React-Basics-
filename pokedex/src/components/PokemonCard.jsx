8f1402a08436fed663569be0a7f200c0adf65741
LAURA_MAYA DD <mayadray1@gmail.com>
2023-11-22 14:35:45 +0100

    try

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -2,7 +2,7 @@ function PokemonCard() {
     return (<figure>
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
         <figcaption>Bulbasaur</figcaption>
-
+        <p>holà</p>
     </figure>
     );
 }
