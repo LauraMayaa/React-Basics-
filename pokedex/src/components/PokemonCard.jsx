4bb390b109406ec686b2ee132f05a466186f2dfe
LAURA_MAYA DD <mayadray1@gmail.com>
2023-11-23 11:27:43 +0100

    done

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,10 +1,35 @@
+import React from 'react';
+
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
 function PokemonCard() {
-    return (<figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-        <p>holà</p>
-    </figure>
+    const pokemon = pokemonList[1]
+
+    return (
+
+        <figure>
+            {
+                pokemon.imgSrc ?
+                    (<img src={pokemon.imgSrc} alt={pokemon.name} />)
+                    :
+                    (<p>???</p>)
+            }
+
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
     );
 }
+
+
 
 export default PokemonCard;
